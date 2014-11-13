cb36b12b78a7dedfb6e498da2dc60f46cbe85ac5
Paul O'Neil <redballoon36@gmail.com>
2014-11-12 20:37:13 -0500

    New wrapper types for msghdr and cmsg

--- source/dubik.d
+++ source/dubik.d
@@ -19,6 +19,7 @@
 import std.conv;
 import std.stdio;
 import std.typecons;
+import std.typetuple;
 import std.c.linux.socket;
 import std.c.linux.rxrpc;
 
@@ -59,131 +60,332 @@ static this()
     LOCALHOST_IP = inet_addr("127.0.0.1");
 }
 
-struct ControlMessage
+struct ControlMessage(T)
 {
-    int level;
-    int type;
-    ubyte[] data;
-
-    this(cmsghdr * hdr) pure
+    static if (!is(T : void))
     {
-        level = hdr.cmsg_level;
-        type = hdr.cmsg_type;
-        data.length = (cast(ubyte*)(hdr) + hdr.cmsg_len) - CMSG_DATA(hdr);
-        data = CMSG_DATA(hdr)[0 .. data.length];
+        private enum PayloadSize = T.sizeof;
+    }
+    else
+    {
+        private enum PayloadSize = 0;
+    }
+    union {
+        cmsghdr hdr = {PayloadSize, 0, 0};
+        // _data is here to ensure that the struct is the right size for
+        // the payload it contains
+        ubyte[CMSG_LEN(PayloadSize)] _data;
     }
 
-    void setData(T)(T value) pure
+    public:
+    size_t totalLength() pure nothrow @nogc const
     {
-        data.length = T.sizeof;
-        (*cast(T*)data.ptr) = value;
+        return _data.length;
+    }
+    size_t dataLength() pure nothrow @nogc const
+    {
+        return PayloadSize;
     }
 
-    // CMSG_ALIGN and CMSG_LEN are not safe, so this isn't either
-    @property package size_t totalLength() pure nothrow @nogc const
+    ref inout(int) level() inout pure nothrow @nogc @safe
     {
-        return CMSG_LEN(data.length);
+        return hdr.cmsg_level;
     }
-    void serialize(ubyte[] sink) pure nothrow const
+    ref inout(int) type() inout pure nothrow @nogc @safe
     {
-        cmsghdr* cmsg = cast(cmsghdr*)sink.ptr;
-        cmsg.cmsg_len = CMSG_LEN(data.length);
-        cmsg.cmsg_level = level;
-        cmsg.cmsg_type = type;
-        CMSG_DATA(cmsg)[0 .. data.length] = data[];
+        return hdr.cmsg_type;
+    }
+
+    static if (!is(T : void))
+    {
+        ref inout(T) data() inout
+        {
+            return *cast(inout(T)*)CMSG_DATA(&hdr);
+        }
+    }
+
+    package void writeLength()
+    {
+        hdr.cmsg_len = PayloadSize;
+    }
+
+    static assert(ControlMessage!T.sizeof == CMSG_LEN(PayloadSize));
+}
+
+struct MessageHeader(ControlMessageTypes...)
+{
+    import std.algorithm : map, sum;
+
+    private msghdr hdr;
+
+    /* Why can't I do this?
+    alias name = hdr.msg_name;
+    alias namelen = hdr.msg_namelen;
+    alias iov = hdr.msg_iov;
+    alias iovlen = hdr.msg_iovlen;
+    alias flags = hdr.msg_flags;
+    */
+    ref void* name() {
+        return hdr.msg_name;
+    }
+    ref socklen_t namelen() {
+        return hdr.msg_namelen;
+    }
+    ref iovec* iov() {
+        return hdr.msg_iov;
+    }
+    ref size_t iovlen() {
+        return hdr.msg_iovlen;
+    }
+    ref int flags() {
+        return hdr.msg_flags;
+    }
+
+    template AlignedSizeof(T)
+    {
+        // CMSG_ALIGN is a Linux extension, but so is AF_RXRPC, so we'll use it
+        // CMSG_SPACE is from bits/socket.h and should be moved to druntime,
+        // where the other macros are
+        static size_t CMSG_SPACE(size_t len) { return CMSG_ALIGN(len) + CMSG_ALIGN(cmsghdr.sizeof); }
+        enum AlignedSizeof = CMSG_SPACE(CMSG_LEN(T.sizeof));
+    }
+
+    // TODO this is probably always an underestimate.
+    static if (ControlMessageTypes.length == 0)
+    {
+        enum default_buffer_length = 0;
+    }
+    else
+    {
+        enum default_buffer_length = sum([staticMap!(AlignedSizeof, ControlMessageTypes)]);
+    }
+
+    // So if default_buffer length were ever correct, I could allocate
+    // a buffer on the stack, and resize it later if I find out it's broken
+    static if (default_buffer_length > 0)
+    {
+        ubyte[default_buffer_length] default_buffer;
+    }
+
+    static MessageHeader!ControlMessageTypes opCall()
+    {
+        MessageHeader!ControlMessageTypes result;
+
+        static if (ControlMessageTypes.length == 0)
+        {
+            result.hdr.msg_control = null;
+            result.hdr.msg_controllen = 0;
+        }
+        else
+        {
+            result.hdr.msg_control = result.default_buffer.ptr;
+            result.hdr.msg_controllen = default_buffer_length;
+            if (!result.checkBufferLength(result.default_buffer[]))
+            {
+                writeln("Allocating a control buffer");
+                ubyte[] cmsgbuf = new ubyte[default_buffer_length * 2];
+                result.hdr.msg_control = cmsgbuf.ptr;
+                result.hdr.msg_controllen = cmsgbuf.length;
+                while (!result.checkBufferLength(cmsgbuf))
+                {
+                    cmsgbuf.length *= 2;
+                }
+            }
+
+            result.hdr.msg_controllen = (cast(ubyte*)&result.ctrl!(ControlMessageTypes.length - 1)()) + CMSG_LEN(ControlMessageTypes[$-1].sizeof) - result.hdr.msg_control;
+        }
+
+        return result;
+    }
+
+    private bool checkBufferLength(ubyte[] cmsgbuf)
+    {
+        cmsghdr * cmsg = CMSG_FIRSTHDR(&hdr);
+        foreach(T; ControlMessageTypes[0 .. $])
+        {
+            cmsg.cmsg_len = CMSG_LEN(ControlMessage!T.PayloadSize);
+            cmsg = CMSG_NXTHDR(&hdr, cmsg);
+            if (cmsg is null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    ref MessageHeader!ControlMessageTypes opAssign(ref MessageHeader!ControlMessageTypes other)
+    {
+        this.hdr = other.hdr;
+        static if (ControlMessageTypes.length > 0)
+        {
+            if (other.hdr.msg_control == other.default_buffer.ptr)
+            {
+                this.hdr.msg_control = this.default_buffer.ptr;
+                this.hdr.msg_controllen = default_buffer_length;
+                this.default_buffer[] = other.default_buffer[];
+            }
+            else
+            {
+                this.hdr.msg_control = other.hdr.msg_control;
+                this.hdr.msg_controllen = other.hdr.msg_controllen;
+            }
+        }
+        return this;
+    }
+
+    ref ControlMessage!(ControlMessageTypes[idx]) ctrl(size_t idx)()
+        if (idx < ControlMessageTypes.length)
+    {
+        cmsghdr * cmsg = CMSG_FIRSTHDR(&hdr);
+        foreach (i; 0 .. idx)
+        {
+            cmsg = CMSG_NXTHDR(&hdr, cmsg);
+        }
+        return *(cast(ControlMessage!(ControlMessageTypes[idx])*)(cmsg));
+    }
+}
+static assert(MessageHeader!().sizeof == msghdr.sizeof);
+
+struct DynamicControlMessage
+{
+    cmsghdr hdr;
+
+    public:
+    size_t totalLength() pure nothrow @nogc const
+    {
+        return hdr.cmsg_len;
+    }
+    size_t dataLength() pure nothrow @nogc const
+    {
+        return hdr.cmsg_len - cmsghdr.sizeof;
+    }
+
+    ref inout(int) level() inout
+    {
+        return hdr.cmsg_level;
+    }
+    ref inout(int) type() inout
+    {
+        return hdr.cmsg_type;
+    }
+
+    ref inout(ControlMessage!T) to(T)() inout
+    in {
+        // TODO should I drop the < and enforce ==?
+        assert(T.sizeof <= dataLength());
+    }
+    body {
+        return *(cast(inout(ControlMessage!T*))&this);
     }
 }
 
-struct ControlMessageList
+struct DynamicMessageHeader
 {
-    private ControlMessage[] _arr;
+    import std.algorithm : map, sum;
 
-    this(msghdr* msg) pure
+    private msghdr hdr;
+    private ubyte[] ctrl_buffer;
+
+    /* Why can't I do this?
+    alias name = hdr.msg_name;
+    alias namelen = hdr.msg_namelen;
+    alias iov = hdr.msg_iov;
+    alias iovlen = hdr.msg_iovlen;
+    alias flags = hdr.msg_flags;
+    */
+    ref inout(void*) name() inout {
+        return hdr.msg_name;
+    }
+    ref inout(socklen_t) namelen() inout {
+        return hdr.msg_namelen;
+    }
+    ref inout(iovec*) iov() inout {
+        return hdr.msg_iov;
+    }
+    ref inout(size_t) iovlen() inout {
+        return hdr.msg_iovlen;
+    }
+    ref inout(int) flags() inout {
+        return hdr.msg_flags;
+    }
+    ulong controllen() {
+        return hdr.msg_controllen;
+    }
+
+    this(size_t ctrl_buffer_length)
     {
-        cmsghdr* current = CMSG_FIRSTHDR(msg);
-        uint counter = 0;
-        while(current != null)
+        ctrl_buffer = new ubyte[ctrl_buffer_length];
+        hdr.msg_control = ctrl_buffer.ptr;
+        hdr.msg_controllen = ctrl_buffer.length;
+    }
+
+    private struct ControlListLooper
+    {
+        private msghdr* hdr;
+
+        public this(msghdr* h)
         {
-            current = CMSG_NXTHDR(msg, current);
-            ++counter;
+            hdr = h;
         }
 
-        _arr.length = counter;
-        current = CMSG_FIRSTHDR(msg);
-        foreach( ref ControlMessage new_msg ; _arr )
+        // TODO change these to inout
+        int opApply(int delegate(ref const DynamicControlMessage) dg) const
         {
-            new_msg = ControlMessage(current);
-            current = CMSG_NXTHDR(msg, current);
+            int result = 0;
+            for (const(cmsghdr)* cmsg = CMSG_FIRSTHDR(hdr);
+                 cmsg !is null && !result;
+                 cmsg = CMSG_NXTHDR(hdr, cmsg))
+            {
+                result = dg(*cast(DynamicControlMessage*)cmsg);
+            }
+            return result;
+        }
+        int opApply(int delegate(ref ulong, ref const DynamicControlMessage) dg) const
+        {
+            int result = 0;
+            const(cmsghdr)* cmsg = CMSG_FIRSTHDR(hdr);
+            for (ulong idx = 0;
+                 cmsg !is null && !result;
+                 ++idx)
+            {
+                result = dg(idx, *cast(const(DynamicControlMessage)*)cmsg);
+                cmsg = CMSG_NXTHDR(hdr, cmsg);
+            }
+            return result;
+        }
+
+        ulong length() const
+        {
+            ulong len = 0;
+            for (const(cmsghdr)* cmsg = CMSG_FIRSTHDR(hdr);
+                 cmsg !is null;
+                 cmsg = CMSG_NXTHDR(cast(msghdr*)hdr, cmsg))
+            {
+                ++len;
+            }
+            return len;
         }
     }
-
-    @property bool empty() @safe pure nothrow @nogc
+    auto ctrl_list()
     {
-        return _arr.length == 0;
+        return ControlListLooper(&hdr);
     }
 
-    @property inout(ControlMessage) front() inout @safe pure nothrow @nogc
+    ref DynamicControlMessage ctrl(size_t idx)
     {
-        return _arr[0];
-    }
-
-    int opApply(int delegate(ref const ControlMessage) dg) const
-    {
-        int result = 0;
-        foreach(msg ; _arr)
+        cmsghdr * cmsg = CMSG_FIRSTHDR(&hdr);
+        for (size_t i = 0; i < idx && cmsg !is null; ++i)
         {
-            result = dg(msg);
-
-            if( result ) break;
+            cmsg = CMSG_NXTHDR(&hdr, cmsg);
         }
-        return result;
-    }
-    int opApply(int delegate(ref ulong, inout ref ControlMessage) dg) inout
-    {
-        int result = 0;
-        foreach(idx, msg ; _arr)
-        {
-            result = dg(idx, msg);
-
-            if( result ) break;
-        }
-        return result;
+        if (cmsg is null)
+            throw new Exception("Out of bounds!");
+        return *(cast(DynamicControlMessage*)(cmsg));
     }
 
-    @property ulong length() @safe pure nothrow @nogc
+    void resetControlLength()
     {
-        return _arr.length;
-    }
-
-    ControlMessage opIndex(uint idx) @safe pure nothrow @nogc
-    {
-        return _arr[idx];
-    }
-
-    void put(ControlMessage msg) @safe pure
-    {
-        _arr ~= msg;
-    }
-
-    size_t totalLength() pure nothrow @nogc
-    {
-        size_t len = 0;
-        foreach( msg ; _arr )
-        {
-            len += msg.totalLength;
-        }
-        return len;
-    }
-
-    void serialize(ubyte[] result) pure nothrow
-    {
-        result.length = totalLength();
-        size_t startPoint = 0;
-        foreach( const ref msg ; _arr )
-        {
-            msg.serialize(result[startPoint .. startPoint + msg.totalLength]);
-            startPoint += msg.totalLength;
-        }
+        hdr.msg_controllen = ctrl_buffer.length;
     }
 }
 
@@ -234,50 +436,34 @@ void ping()
     }
 
     {
-        ControlMessageList ctrlMsgList;
-        ControlMessage ctrlMsg;
-        ctrlMsg.level = SOL_RXRPC;
-        ctrlMsg.type = RXRPC_USER_CALL_ID;
-        ctrlMsg.setData!ulong(1);
-        ctrlMsgList.put(ctrlMsg);
-        // ubyte[ctrlMsgList.totalLength] control;
-        // DMD reports that the length is not an expression, which is wrong.
-        // It's not a compile time expression , so it doesn't work when we
-        // add the () at the end, but at the very least the message is not good.
-        ubyte[] control;
-        control.length = ctrlMsgList.totalLength;
-        ctrlMsgList.serialize(control);
+        auto msg = MessageHeader!ulong();
+        msg.ctrl!0.level = SOL_RXRPC;
+        msg.ctrl!0.type = RXRPC_USER_CALL_ID;
+        msg.ctrl!0.data = 1;
 
         string msg_string = "PING";
         iovec msg_contents = { cast(void*)msg_string.ptr, msg_string.length };
-        msghdr msg;
-        msg.msg_name = &target_addr;
-        msg.msg_namelen = sockaddr_rxrpc.sizeof;
-        msg.msg_iov = &msg_contents;
-        msg.msg_iovlen = 1;
-        msg.msg_control = control.ptr;
-        msg.msg_controllen = ctrlMsgList.totalLength;
-        msg.msg_flags = 0;
+        msg.name = &target_addr;
+        msg.namelen = sockaddr_rxrpc.sizeof;
+        msg.iov = &msg_contents;
+        msg.iovlen = 1;
+        msg.flags = 0;
 
-        ssize_t success = sendmsg(send_socket, &msg, 0);
+        ssize_t success = sendmsg(send_socket, cast(msghdr*)&msg, 0);
         assert(success == msg_string.length);
     }
 
     {
         ubyte[128] msg_string;
-        ubyte[128] control;
-        socklen_t controllen = control.length;
         iovec msg_contents = { cast(void*)msg_string.ptr, msg_string.length };
-        msghdr msg;
-        msg.msg_name = null;
-        msg.msg_namelen = 0;
-        msg.msg_iov = &msg_contents;
-        msg.msg_iovlen = 1;
-        msg.msg_control = control.ptr;
-        msg.msg_controllen = controllen;
-        msg.msg_flags = 0;
+        DynamicMessageHeader msg = DynamicMessageHeader(128);
+        msg.name = null;
+        msg.namelen = 0;
+        msg.iov = &msg_contents;
+        msg.iovlen = 1;
+        msg.flags = 0;
 
-        ssize_t success = recvmsg(send_socket, &msg, 0);
+        ssize_t success = recvmsg(send_socket, cast(msghdr*)&msg, 0);
         writeln("recvmsg = ", success, " ", errno);
         if (success > 0) {
             writeln("Got message from server: ",
@@ -288,28 +474,21 @@ void ping()
 
 // XXX To a library with thee!
 void rxrpc_accept(int socket, ulong id) {
-    ubyte[128] cb;
-    uint cl = 0;
-    msghdr msg;
+    auto msg = MessageHeader!(void, ulong)();
 
     {
-        cmsghdr * cmsg = cast(cmsghdr*)(cb.ptr + cl);
-        cmsg.cmsg_len  = CMSG_LEN(0);
-        cmsg.cmsg_level = SOL_RXRPC;
-        cmsg.cmsg_type = RXRPC_ACCEPT;
-        cl += cmsg.cmsg_len;
+        msg.ctrl!0.level = SOL_RXRPC;
+        msg.ctrl!0.type = RXRPC_ACCEPT;
     }
-    addCallID(cb, id, cl);
+    msg.setCallID!1(id);
 
-    msg.msg_name = null;
-    msg.msg_namelen = 0;
-    msg.msg_iov = null;
-    msg.msg_iovlen = 0;
-    msg.msg_control = cb.ptr;
-    msg.msg_controllen = cl;
-    msg.msg_flags = 0;
+    msg.name = null;
+    msg.namelen = 0;
+    msg.iov = null;
+    msg.iovlen = 0;
+    msg.flags = 0;
 
-    ssize_t success = sendmsg(socket, &msg, 0);
+    ssize_t success = sendmsg(socket, cast(msghdr*)&msg, 0);
     writefln("RXRPC ACCEPT sendmsg = %d %d", success, errno);
 }
 
@@ -321,26 +500,29 @@ struct RXRPCall {
 };
 
 
+void setCallID(int idx, MessageHeader)(ref MessageHeader msg, ulong call_id)
+{
+    msg.ctrl!idx.level = SOL_RXRPC;
+    msg.ctrl!idx.type = RXRPC_USER_CALL_ID;
+    msg.ctrl!idx.data = call_id;
+}
+
 void pong(RXRPCall *c, ubyte[] arg) {
     writeln("PONG MESSAGE: ", c, ":", arg);
 
-    ubyte[128] control;
-    uint controllen = 0;
-    addCallID(control, cast(ulong)c, controllen);
+    auto msg = MessageHeader!ulong();
+    setCallID!0(msg, cast(ulong)c);
 
     ubyte[] msg_string = cast(ubyte[])("PONG!  Thanks for using RX!");
     iovec msg_contents = { cast(void*)msg_string.ptr, msg_string.length };
 
-    msghdr msg;
-    msg.msg_name = null;
-    msg.msg_namelen = 0;
-    msg.msg_iovlen = 1;
-    msg.msg_iov = &msg_contents;
-    msg.msg_control = control.ptr;
-    msg.msg_controllen = controllen;
-    msg.msg_flags = 0;
+    msg.name = null;
+    msg.namelen = 0;
+    msg.iovlen = 1;
+    msg.iov = &msg_contents;
+    msg.flags = 0;
 
-    ssize_t success = sendmsg(c.socket, &msg, 0);
+    ssize_t success = sendmsg(c.socket, cast(msghdr*)&msg, 0);
     writeln("PONG SENDMSG: ", success, " ", errno);
 }
 
@@ -369,18 +551,15 @@ void server()
 
     listen(server_socket, 100);
 
-    ubyte[128] control;
-    uint controllen = 0;
-
     ubyte[128] msg_string;
     iovec msg_contents = { cast(void*)msg_string.ptr, msg_string.length };
 
     ubyte[1024] msg_name;
-    msghdr msg;
+    DynamicMessageHeader msg = DynamicMessageHeader(128);
 
-    msg.msg_iovlen = 1;
-    msg.msg_iov = &msg_contents;
-    msg.msg_flags = 0;
+    msg.iovlen = 1;
+    msg.iov = &msg_contents;
+    msg.flags = 0;
 
     // Wait for a message
     writeln("Looping...");
@@ -389,28 +568,27 @@ void server()
     poll_info.events = POLLIN;
     while(poll(&poll_info, 1, -1)) {
 
-        msg.msg_name = msg_name.ptr;
-        msg.msg_namelen = msg_name.length;
-        msg.msg_iov.iov_len = msg_string.length;
-        msg.msg_control = control.ptr;
-        msg.msg_controllen = control.length;
+        msg.name = msg_name.ptr;
+        msg.namelen = msg_name.length;
+        msg.iov.iov_len = msg_string.length;
+        msg.resetControlLength();
+        /*msg.control = control.ptr;
+        msg.controllen = control.length;*/
 
-        ssize_t success = recvmsg(server_socket, &msg, 0);
+        ssize_t success = recvmsg(server_socket, cast(msghdr*)&msg, 0);
         if( success == -1 )
         {
             writeln("Receive failed!");
             writefln("Errno = %d", errno);
             return;
         }
-        writeln("Success = ", success, " ", msg.msg_controllen);
+        writeln("Success = ", success, " ", msg.controllen);
 
-        ControlMessageList ctrl_msg_list = ControlMessageList(&msg);
-
-        writefln("CTRL MSGS = %d", ctrl_msg_list.length);
+        writefln("CTRL MSGS = %d", msg.ctrl_list.length);
         Nullable!ulong this_call;
         Nullable!long this_abort;
         bool this_finack;
-        foreach( ctrl_msg ; ctrl_msg_list )
+        foreach (ref const DynamicControlMessage ctrl_msg ; msg.ctrl_list)
         {
             writefln("CTRL MSG: %d %d %d",
                 ctrl_msg.level,
@@ -421,6 +599,7 @@ void server()
             switch(ctrl_msg.type) {
                 case RXRPC_NEW_CALL:
                     auto c = new RXRPCall;
+                    writeln("new call c = ", c);
                     c.socket  = server_socket;
                     c.cb_data = &pong;
                     c.cb_finalack = &pongfa;
@@ -431,13 +610,14 @@ void server()
                     rxrpc_accept(server_socket, cast(ulong)c);
                     break;
                 case RXRPC_USER_CALL_ID:
-                    this_call = *cast(ulong*)(ctrl_msg.data.ptr);
+                    this_call = ctrl_msg.to!ulong().data;
                     break;
                 case RXRPC_ACK:
                     this_finack = true;
                     break;
                 case RXRPC_ABORT:
-                    this_abort = *cast(long*)(ctrl_msg.data.ptr);
+                    writeln("Get the abort.");
+                    this_abort = ctrl_msg.to!long.data;
                     break;
                 default:
                     writeln("  Unknown control message");
